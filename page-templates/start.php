b0aca34a822e85f367588accb5abf8c43bcdf875
JB <info@jonasbuck.com>
2019-04-29 00:35:04 +0200

    cleanup _content.scss

--- page-templates/start.php
+++ page-templates/start.php
@@ -1,18 +1,84 @@
 <?php
 /*
-Template Name: Full Width
+Template Name: Startseite
 */
 get_header(); ?>
 
-<?php get_template_part( 'template-parts/featured-image' ); ?>
-<div class="main-container">
-	<div class="main-grid">
-		<main class="main-content-full-width">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-				<?php comments_template(); ?>
-			<?php endwhile; ?>
-		</main>
+<div class="bg-dark-grey">
+	<div class="grid-container fluid">
+	<div class="grid-x small-up-1 medium-up-2 large-up-3">
+		<div class="cell">
+		<a href="#">
+			<div class="image">
+			<img src="https://source.unsplash.com/A-NVHPka9Rk" />
+			</div>
+			<h2>Nulla quis sem at nibh elementum imperdiet</h2>
+			<div class="summary">
+			<p>Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt
+				mattis</p>
+			</div>
+		</a>
+		</div>
+		<div class="cell">
+		<a href="#">
+			<div class="image">
+			<img src="https://source.unsplash.com/CLm3pWXrS9Q" />
+			</div>
+			<h2>Class aptent taciti sociosqu ad litora torquent per conubia nostra </h2>
+			<div class="summary">
+			<p>Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Proin ut ligula vel nunc egestas porttitor.
+				Morbi lectus risus, iaculis vel, suscipit quis </p>
+			</div>
+		</a>
+		</div>
+		<div class="cell">
+		<a href="#">
+			<div class="image">
+			<img src="https://source.unsplash.com/MctEgCk1Dm0" />
+			</div>
+			<h2> Proin ut ligula vel nunc egestas porttitor</h2>
+			<div class="summary">
+			<p>Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis
+				in faucibus litora torquent per conubia</p>
+			</div>
+		</a>
+		</div>
+		<div class="cell">
+		<a href="#">
+			<div class="image">
+			<img src="https://source.unsplash.com/W9OKrxBqiZA" />
+			</div>
+			<h2>Quisque nisl felis, venenatis tristique, dignissim in, ultrices</h2>
+			<div class="summary">
+			<p>Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue</p>
+			</div>
+		</a>
+		</div>
+		<div class="cell">
+		<a href="#">
+			<div class="image">
+			<img src="https://source.unsplash.com/d44Vd16m0LM" />
+			</div>
+			<h2>Donec lacus nunc, viverra nec tortor egestas</h2>
+			<div class="summary">
+			<p>Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc</p>
+			</div>
+		</a>
+		</div>
+		<div class="cell">
+		<a href="#">
+			<div class="image">
+			<img src="https://source.unsplash.com/PUvPZckRnOg" />
+			</div>
+			<h2>Fusce nec tellus sed augue semper porta</h2>
+			<div class="summary">
+			<p>Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam. Aenean laoreet. Vestibulum nisi lectus, commodo ac, facilisis ac, ultricies eu, pede. Ut orci risus, accumsan
+				porttitor, cursus quis, aliquet eget, justo. </p>
+			</div>
+		</a>
+		</div>
+	</div>
 	</div>
 </div>
+
 <?php get_footer();
