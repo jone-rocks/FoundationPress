29e452c763c3efa650982033a2c48b5abd40637f
JB <info@jonasbuck.com>
2019-04-29 16:35:01 +0200

    startseite - first content

--- page-templates/start.php
+++ page-templates/start.php
@@ -4,81 +4,91 @@ Template Name: Startseite
 */
 get_header(); ?>
 
-<div class="bg-dark-grey">
-	<div class="grid-container fluid">
-	<div class="grid-x small-up-1 medium-up-2 large-up-3">
+<!-- Full width image -->
+<div class="grid-container full start-img">
+	<div class="grid-x">
 		<div class="cell">
-		<a href="#">
-			<div class="image">
-			<img src="https://source.unsplash.com/A-NVHPka9Rk" />
-			</div>
-			<h2>Nulla quis sem at nibh elementum imperdiet</h2>
-			<div class="summary">
-			<p>Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt
-				mattis</p>
-			</div>
-		</a>
+			<img src="<?php echo get_template_directory_uri() ?>/img/full-bg-img.jpg" />
 		</div>
-		<div class="cell">
-		<a href="#">
-			<div class="image">
-			<img src="https://source.unsplash.com/CLm3pWXrS9Q" />
-			</div>
-			<h2>Class aptent taciti sociosqu ad litora torquent per conubia nostra </h2>
-			<div class="summary">
-			<p>Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Proin ut ligula vel nunc egestas porttitor.
-				Morbi lectus risus, iaculis vel, suscipit quis </p>
-			</div>
-		</a>
-		</div>
-		<div class="cell">
-		<a href="#">
-			<div class="image">
-			<img src="https://source.unsplash.com/MctEgCk1Dm0" />
-			</div>
-			<h2> Proin ut ligula vel nunc egestas porttitor</h2>
-			<div class="summary">
-			<p>Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis
-				in faucibus litora torquent per conubia</p>
-			</div>
-		</a>
-		</div>
-		<div class="cell">
-		<a href="#">
-			<div class="image">
-			<img src="https://source.unsplash.com/W9OKrxBqiZA" />
-			</div>
-			<h2>Quisque nisl felis, venenatis tristique, dignissim in, ultrices</h2>
-			<div class="summary">
-			<p>Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue</p>
-			</div>
-		</a>
-		</div>
-		<div class="cell">
-		<a href="#">
-			<div class="image">
-			<img src="https://source.unsplash.com/d44Vd16m0LM" />
-			</div>
-			<h2>Donec lacus nunc, viverra nec tortor egestas</h2>
-			<div class="summary">
-			<p>Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc</p>
-			</div>
-		</a>
-		</div>
-		<div class="cell">
-		<a href="#">
-			<div class="image">
-			<img src="https://source.unsplash.com/PUvPZckRnOg" />
-			</div>
-			<h2>Fusce nec tellus sed augue semper porta</h2>
-			<div class="summary">
-			<p>Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam. Aenean laoreet. Vestibulum nisi lectus, commodo ac, facilisis ac, ultricies eu, pede. Ut orci risus, accumsan
-				porttitor, cursus quis, aliquet eget, justo. </p>
-			</div>
-		</a>
-		</div>
-	</div>
 	</div>
 </div>
+
+<!-- Dark BG Header -->
+<div class="bg-dark-grey">
+	<div class="grid-container">
+		<div class="grid-x grid-padding-y small-up-1 medium-up-2 large-up-2">
+			<div class="cell">
+				<h1>Ihre IT in besten Händen</h1>
+				<h2>Raab IT-Systemhaus - seit 1988</h2>
+			</div>
+			<div class="cell">
+				<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- Stärken und Vorteile -->
+<div class="grid-container">
+	<div class="grid-x grid-padding-y align-center">
+		<div class="cell small-8 text-center">
+			<h3 class="color-red">Stärken und Vorteile</h3>
+			<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed</p>
+		</div>
+	</div>
+	<div class="grid-x grid-padding-y staerken">
+		<div class="cell small-3 text-center">
+			Kompetenz
+		</div>
+		<div class="cell small-3 text-center">
+			Persönlichkeit
+		</div>
+		<div class="cell small-3 text-center">
+			Regional
+		</div>
+		<div class="cell small-3 text-center">
+			Zuverlässigleit
+		</div>
+	</div>
+</div>
+
+<!-- Geschäftsführung -->
+<div class="grid-container">
+	<div class="grid-x">
+		<div class="cell small-5 bg-dark-grey padding-small">
+			<h3>Geschäftsführung</h3>
+			<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed</p>
+		</div>
+		<div class="cell small-7">
+			<img src="<?php echo get_template_directory_uri() ?>/img/gesch-img.png" />
+		</div>
+	</div>
+</div>
+
+<!-- Referenzen-->
+<div class="grid-container">
+	<div class="grid-x grid-padding-y align-center">
+		<div class="cell small-8 text-center">
+			<h3 class="color-red">Stärken und Vorteile</h3>
+			<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed</p>
+		</div>
+	</div>
+	<div class="grid-x grid-padding-y staerken">
+		<div class="cell small-3 text-center">
+			Kompetenz
+		</div>
+		<div class="cell small-3 text-center">
+			Persönlichkeit
+		</div>
+		<div class="cell small-3 text-center">
+			Regional
+		</div>
+		<div class="cell small-3 text-center">
+			Zuverlässigleit
+		</div>
+	</div>
+</div>
+
+
 
 <?php get_footer();
