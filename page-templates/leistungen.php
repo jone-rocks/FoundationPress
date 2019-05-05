7807fdf16bd325a68bc98bf5240e6c6fabd360c1
JB <info@jonasbuck.com>
2019-05-05 22:29:55 +0200

    Blank templates kontakt, leistungen, support

---
+++ page-templates/leistungen.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Template Name: Leistungen
+*/
+get_header(); ?>
+
+<!-- Full width image -->
+<div class="grid-container full start-img">
+	<div class="grid-x">
+		<div class="cell">
+			<img src="<?php echo get_template_directory_uri() ?>/img/full-bg-img.jpg" />
+		</div>
+	</div>
+</div>
+
+<?php get_footer();
