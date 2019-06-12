2f99887c271c699609a22f8a6105bf9bdfc6255d
JB <info@jonasbuck.com>
2019-06-12 23:35:58 +0200

    Navigation

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
 			<div class="title-bar-left">
 				<button aria-label="<?php _e( 'Main Menu', 'foundationpress' ); ?>" class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
 				<span class="site-mobile-title title-bar-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>title-bar-left
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 				</span>
 			</div>
 		</div>
@@ -37,22 +37,11 @@
 		<nav class="site-navigation top-bar" role="navigation" id="<?php foundationpress_mobile_menu_id(); ?>">
 			<div class="top-bar-left">
 				<div class="site-desktop-title top-bar-title">
-					<img class="logo" src="<?php echo get_template_directory_uri() ?>/img/raab_logo.png" alt="">
+					
 				</div>
 			</div>
 			<div class="top-bar-right">
-				<ul class="dropdown menu" data-dropdown-menu>
-					<li>
-						<a href="#">Item 1</a>
-						<ul class="menu">
-						<li><a href="#">Item 1A</a></li>
-						<!-- ... -->
-						</ul>
-					</li>
-					<li><a href="#">Item 2</a></li>
-					<li><a href="#">Item 3</a></li>
-					<li><a href="#">Item 4</a></li>
-				</ul>
+				<?php wp_nav_menu( array ('menu' => 'nav', 'menu_class' => 'dropdown menu' , 'container' => '', 'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',) ) ?>
 			</div>
 		</nav>
 
