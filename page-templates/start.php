79cd53ad7f4994449a7dc5d59fb423ac9b8c8e4b
JB <info@jonasbuck.com>
2019-07-14 00:58:27 +0200

    Startseite

--- page-templates/start.php
+++ page-templates/start.php
@@ -16,7 +16,7 @@ get_header(); ?>
 <!-- Dark BG Header -->
 <div class="bg-dark-grey">
 	<div class="grid-container">
-		<div class="grid-x grid-padding-y small-up-1 medium-up-2 large-up-2">
+		<div class="grid-x grid-padding-y grid-padding-x align-middle small-up-1 medium-up-2 large-up-2 startseite-header">
 			<div class="cell">
 				<h1><?php the_field('startseite_title'); ?></h1>
 				<h2><?php the_field('startseite_header'); ?></h2>
@@ -30,27 +30,27 @@ get_header(); ?>
 
 <!-- Stärken und Vorteile -->
 <div class="grid-container">
-	<div class="grid-x grid-padding-y align-center">
+	<div class="grid-x align-center headline">
 		<div class="cell small-8 text-center">
 			<h3 class="color-red"><?php the_field('staerken_title'); ?></h3>
 			<p><?php the_field('staerken_text'); ?></p>
 		</div>
 	</div>
-	<div class="grid-x grid-padding-y staerken">
+	<div class="grid-x staerken">
 		<div class="cell small-3 text-center">
-			<?php echo wp_get_attachment_image( get_field('startseite_header_img'), 'full' ); ?><br />
+			<?php echo wp_get_attachment_image( get_field('staerken_img_1'), 'full' ); ?><br />
 			<?php the_field('staerken_imgtxt_1'); ?>
 		</div>
 		<div class="cell small-3 text-center">
-			<?php echo wp_get_attachment_image( get_field('startseite_header_img'), 'full' ); ?><br />
+			<?php echo wp_get_attachment_image( get_field('staerken_img_2'), 'full' ); ?><br />
 			<?php the_field('staerken_imgtxt_2'); ?>
 		</div>
 		<div class="cell small-3 text-center">
-			<?php echo wp_get_attachment_image( get_field('startseite_header_img'), 'full' ); ?><br />	
+			<?php echo wp_get_attachment_image( get_field('staerken_img_3'), 'full' ); ?><br />	
 			<?php the_field('staerken_imgtxt_3'); ?>
 		</div>
 		<div class="cell small-3 text-center">
-			<?php echo wp_get_attachment_image( get_field('startseite_header_img'), 'full' ); ?><br />
+			<?php echo wp_get_attachment_image( get_field('staerken_img_4'), 'full' ); ?><br />
 			<?php the_field('staerken_imgtxt_4'); ?>
 		</div>
 	</div>
@@ -58,7 +58,7 @@ get_header(); ?>
 
 <!-- Geschäftsführung -->
 <div class="grid-container">
-	<div class="grid-x">
+	<div class="grid-x geschaeftsfuehrung">
 		<div class="cell small-5 bg-dark-grey padding-small">
 			<h3><?php the_field('geschaeftsfuehrung_header'); ?></h3>
 			<p><?php the_field('geschaeftsfuehrung_txt'); ?></p>
@@ -71,7 +71,7 @@ get_header(); ?>
 
 <!-- Referenzen-->
 <div class="grid-container">
-	<div class="grid-x grid-padding-y align-center">
+	<div class="grid-x grid-padding-y align-center headline">
 		<div class="cell small-8 text-center">
 			<h3 class="color-red"><?php the_field('referenzen_header'); ?></h3>
 			<p><?php the_field('referenzen_txt'); ?></p>
@@ -92,13 +92,13 @@ get_header(); ?>
 
 <!-- Leistungen -->
 <div class="grid-container">
-	<div class="grid-x grid-padding-y align-center">
+	<div class="grid-x align-center leistungen-header">
 		<div class="cell text-center">
 			<h3 class="color-red"><?php the_field('leistungen_header'); ?></h3>
 		</div>
 	</div>
 
-	<div class="grid-x grid-padding-y grid-margin-x grid-margin-y leistungen">
+	<div class="grid-x grid-margin-x grid-margin-y leistungen">
 		<?php
 			if( have_rows('leistungen_tile') ):
 				while ( have_rows('leistungen_tile') ) : the_row();?>
