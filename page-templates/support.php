4511857af0d99d1f89fc87a38f8f3096a48bb5c1
JB <info@jonasbuck.com>
2019-07-14 20:17:27 +0200

    Support (ACF support)

--- page-templates/support.php
+++ page-templates/support.php
@@ -8,7 +8,7 @@ get_header(); ?>
 <div class="grid-container full start-img">
 	<div class="grid-x">
 		<div class="cell">
-			<img src="<?php echo get_template_directory_uri() ?>/img/full-bg-img.jpg" />
+			<?php echo wp_get_attachment_image( get_field('support_header'), 'full' ); ?>
 		</div>
 	</div>
 </div>
@@ -16,13 +16,13 @@ get_header(); ?>
 <!-- Dark BG Header -->
 <div class="bg-dark-grey">
 	<div class="grid-container">
-		<div class="grid-x grid-padding-y small-up-1 medium-up-2 large-up-2">
+		<div class="grid-x grid-padding-y grid-padding-x align-middle small-up-1 medium-up-2 large-up-2 dark-header">
 			<div class="cell">
-				<h1>Headline 1</h1>
-				<h2>Headline 2</h2>
+				<h1><?php the_field('support_title'); ?></h1>
+				<h2><?php the_field('support_title2'); ?></h2>
 			</div>
 			<div class="cell">
-				<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed</p>
+				<p><?php the_field('support_titletext'); ?></p>
 			</div>
 		</div>
 	</div>
@@ -31,7 +31,7 @@ get_header(); ?>
 <!-- 24/7 -->
 <div class="bg-red">
 	<div class="grid-container">
-		<div class="grid-x grid-padding-y align-center">
+		<div class="grid-x align-center s24-7">
 			<div class="cell small-8 text-center">
 				Reaktion unter 2 Stunden <img class="support_availability" src="<?php echo get_template_directory_uri() ?>/img/stopwatch.png" /> 24 Stunden - 7 Tage
 			</div>
@@ -41,36 +41,51 @@ get_header(); ?>
 
 <!-- Rufbereitschaft -->
 <div class="grid-container rufbereitschaft">
-	<div class="grid-x grid-padding-y align-center">
+	<div class="grid-x align-center headline">
 		<div class="cell small-8 text-center">
-			<h3 class="color-red">H3 Headline - Rufbereitschaft</h3>
-			<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy t dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed</p>
+			<h3 class="color-red"><?php the_field('rufbereitschaft_headline'); ?></h3>
+			<p><?php the_field('rufbereitschaft_text'); ?></p>
 		</div>
 	</div>
 	<div class="grid-x grid-padding-y grid-margin-x check-row">
 		<div class="cell small-4 large-offset-2 border-bottom-lightgrey">
-			<img class="check" src="<?php echo get_template_directory_uri() ?>/img/haken_rot_auf_weiß.png" /> Lorem ipsum dolor sit amet
+			<img class="check" src="<?php echo get_template_directory_uri() ?>/img/haken_rot_auf_weiß.png" /> <?php the_field('rufbereitschaft_haken1'); ?>
 		</div>
 		<div class="cell small-4 border-bottom-lightgrey">
-			<img class="check" src="<?php echo get_template_directory_uri() ?>/img/haken_rot_auf_weiß.png" /> consetetur sadipscing elitr
+			<img class="check" src="<?php echo get_template_directory_uri() ?>/img/haken_rot_auf_weiß.png" /> <?php the_field('rufbereitschaft_haken2'); ?>
 		</div>
 	</div>
 	<div class="grid-x grid-padding-y grid-margin-x check-row">
 		<div class="cell small-4 large-offset-2">
-			<img class="check" src="<?php echo get_template_directory_uri() ?>/img/haken_rot_auf_weiß.png" /> sed diam nonumy eirmod tempor
+			<img class="check" src="<?php echo get_template_directory_uri() ?>/img/haken_rot_auf_weiß.png" /> <?php the_field('rufbereitschaft_haken3'); ?>
 		</div>
 		<div class="cell small-4">
-			<img class="check" src="<?php echo get_template_directory_uri() ?>/img/haken_rot_auf_weiß.png" /> eirmod tempor invidunt ut labore e
+			<img class="check" src="<?php echo get_template_directory_uri() ?>/img/haken_rot_auf_weiß.png" /> <?php the_field('rufbereitschaft_haken4'); ?>
 		</div>
 	</div>
 	<div class="grid-x grid-padding-y mail-phone">
 		<div class="cell small-2 large-offset-4 text-center border-right">
-			<img class="check" src="http://cdn.onlinewebfonts.com/svg/img_132401.png" /><br />
+			<img src="<?php echo get_template_directory_uri() ?>/img/mail-icon.png" /><br />
 			<a href="mailto:info@Raab-IT.de">info@Raab-IT.de</a>
 		</div>
 		<div class="cell small-2 text-center">
-			<img class="check" src="http://cdn.onlinewebfonts.com/svg/img_132401.png" /><br />
+			<img src="<?php echo get_template_directory_uri() ?>/img/phone-icon.png" /><br />
 			<a href="call:+498001013398">08001013398</a>
+		</div>
+	</div>
+</div>
+
+<!-- Klaus Raab -->
+<div class="bg-light-grey">
+	<div class="grid-container">
+		<div class="grid-x grid-padding-x align-middle klaus-raab">
+			<div class="cell small-7">
+				<?php echo wp_get_attachment_image( get_field('zitat_bild'), 'full' ); ?>
+			</div>
+			<div class="cell small-5 padding-small quote">
+				<p><?php the_field('rufbereitschaft_haken3'); ?></p>
+				<strong>Klaus Raab, Geschäftsführer</strong>
+			</div>
 		</div>
 	</div>
 </div>
@@ -78,26 +93,26 @@ get_header(); ?>
 <!-- Benutzerservice -->
 <div class="bg-light-grey">
 	<div class="grid-container benutzerservice">
-		<div class="grid-x grid-padding-y align-center">
+		<div class="grid-x align-center headline">
 			<div class="cell small-8 text-center">
-				<h3 class="color-red">H3 Headline - Benutzerservice</h3>
-				<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy t dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed</p>
+				<h3 class="color-red"><?php the_field('benutzerservice_headline'); ?></h3>
+				<p><?php the_field('benutzerservice_text'); ?></p>
 			</div>
 		</div>
 		<div class="grid-x grid-padding-y grid-margin-x check-row">
 			<div class="cell small-4 large-offset-2 border-bottom-red">
-				<img class="check" src="<?php echo get_template_directory_uri() ?>/img/haken_rot_auf_weiß.png" /> Lorem ipsum dolor sit amet
+				<img class="check" src="<?php echo get_template_directory_uri() ?>/img/haken_rot_auf_weiß.png" /> <?php the_field('Benutzerservice_haken1'); ?>
 			</div>
 			<div class="cell small-4  border-bottom-red">
-				<img class="check" src="<?php echo get_template_directory_uri() ?>/img/haken_rot_auf_weiß.png" /> consetetur sadipscing elitr
+				<img class="check" src="<?php echo get_template_directory_uri() ?>/img/haken_rot_auf_weiß.png" /> <?php the_field('Benutzerservice_haken2'); ?>
 			</div>
 		</div>
 		<div class="grid-x grid-padding-y grid-margin-x check-row">
 			<div class="cell small-4 large-offset-2">
-				<img class="check" src="<?php echo get_template_directory_uri() ?>/img/haken_rot_auf_weiß.png" /> sed diam nonumy eirmod tempor
+				<img class="check" src="<?php echo get_template_directory_uri() ?>/img/haken_rot_auf_weiß.png" /> <?php the_field('Benutzerservice_haken3'); ?>
 			</div>
 			<div class="cell small-4">
-				<img class="check" src="<?php echo get_template_directory_uri() ?>/img/haken_rot_auf_weiß.png" /> eirmod tempor invidunt ut labore e
+				<img class="check" src="<?php echo get_template_directory_uri() ?>/img/haken_rot_auf_weiß.png" /> <?php the_field('Benutzerservice_haken4'); ?>
 			</div>
 		</div>
 	</div>
