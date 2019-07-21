e334fd3e36f491b40443fa2c3cbd508ad14cd3da
JB <info@jonasbuck.com>
2019-07-21 22:01:19 +0200

    Sub Navigation

--- header.php
+++ header.php
@@ -57,7 +57,10 @@
 						</div>
 					</div>
 					<div class="top-bar-right">
-						<?php wp_nav_menu( array ('menu' => 'nav', 'menu_class' => 'dropdown menu' , 'container' => '', 'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',) ) ?>
+						<?php foundationpress_top_bar_r(); ?>
+						<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
+							<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
+						<?php endif; ?>
 					</div>
 				</nav>
 			</div>
