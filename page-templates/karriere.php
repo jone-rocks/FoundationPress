5c61de9d64b7f21aa475ef573513231f3cdbcf06
JB <info@jonasbuck.com>
2019-08-10 22:17:53 +0200

    Karriere

--- page-templates/karriere.php
+++ page-templates/karriere.php
@@ -8,113 +8,59 @@ get_header(); ?>
 <div class="grid-container full start-img">
 	<div class="grid-x">
 		<div class="cell">
-			<?php echo wp_get_attachment_image( get_field('support_header'), 'full' ); ?>
+			<?php echo wp_get_attachment_image( get_field('karriere_header'), 'full' ); ?>
 		</div>
 	</div>
 </div>
 
 <!-- Dark BG Header -->
-<div class="bg-dark-grey">
-	<div class="grid-container">
-		<div class="grid-x grid-padding-y grid-padding-x align-middle small-up-1 medium-up-2 large-up-2 dark-header">
-			<div class="cell">
-				<h1><?php the_field('support_title'); ?></h1>
-				<h2><?php the_field('support_title2'); ?></h2>
-			</div>
-			<div class="cell">
-				<p><?php the_field('support_titletext'); ?></p>
-			</div>
-		</div>
-	</div>
-</div>
-
-<!-- 24/7 -->
-<div class="bg-red">
-	<div class="grid-container">
-		<div class="grid-x align-center s24-7">
-			<div class="cell small-8 text-center">
-				Reaktion unter 2 Stunden <img class="support_availability" src="<?php echo get_template_directory_uri() ?>/img/stopwatch.png" /> 24 Stunden - 7 Tage
+<?php if( have_rows('title') ): 
+	while( have_rows('title') ): the_row(); ?>
+		<div class="bg-dark-grey"><a id="consulting"></a>
+			<div class="grid-container">
+				<div class="grid-x grid-padding-x align-middle small-up-1 medium-up-2 large-up-2 dark-header">
+					<div class="cell">
+						<h1><?php the_sub_field('header'); ?></h1>
+						<h2><?php the_sub_field('subheader'); ?></h2>
+					</div>
+					<div class="cell">
+						<p><?php the_sub_field('text'); ?></p>
+					</div>
+				</div>
 			</div>
 		</div>
-	</div>
-</div>
+	<?php endwhile; ?>
+<?php endif; ?>
 
-<!-- Rufbereitschaft -->
-<div class="grid-container rufbereitschaft">
+
+
+<!-- Stellenangebote -->
+<div class="grid-container stellenangebote">
 	<div class="grid-x align-center padding-x-80">
-		<div class="cell small-8 text-center">
-			<h3 class="color-red"><?php the_field('rufbereitschaft_headline'); ?></h3>
-			<p><?php the_field('rufbereitschaft_text'); ?></p>
+		<div class="cell small-12 medium-4 karriere_ansprechpartner">
+			<h3 class="color-red"><?php the_field('ansprechpartner_header'); ?></h3>
+			<?php echo wp_get_attachment_image( get_field('ansprechpartner_img'), 'full' ); ?>
+			<p><?php the_field('ansprechpartner_kontaktinformationen'); ?></p>
 		</div>
-	</div>
-	<div class="grid-x grid-padding-y grid-margin-x check-row">
-		<div class="cell small-4 large-offset-2 border-bottom-lightgrey">
-			<img class="check" src="<?php echo get_template_directory_uri() ?>/img/raabit_haken_aufzaehlung.svg" /> <?php the_field('rufbereitschaft_haken1'); ?>
+		<?php
+		if( have_rows('stellenangebote') ): ?>
+		<div class="cell small-12 medium-8 stellenangebot-accordion">
+			<ul class="accordion" data-accordion>
+			<?php while ( have_rows('stellenangebote') ) : the_row(); ?>
+				<li class="accordion-item" data-accordion-item>
+					<a href="#" class="accordion-title"><span class="accordion-circle"></span><?php the_sub_field('stelle'); ?></a>
+					<div class="accordion-content" data-tab-content>
+						<p><?php the_sub_field('stellenbeschreibung'); ?></p>
+					</div>
+				</li>
+				<?php endwhile; ?>
+			</ul>
 		</div>
-		<div class="cell small-4 border-bottom-lightgrey">
-			<img class="check" src="<?php echo get_template_directory_uri() ?>/img/raabit_haken_aufzaehlung.svg" /> <?php the_field('rufbereitschaft_haken2'); ?>
-		</div>
-	</div>
-	<div class="grid-x grid-padding-y grid-margin-x check-row">
-		<div class="cell small-4 large-offset-2">
-			<img class="check" src="<?php echo get_template_directory_uri() ?>/img/raabit_haken_aufzaehlung.svg" /> <?php the_field('rufbereitschaft_haken3'); ?>
-		</div>
-		<div class="cell small-4">
-			<img class="check" src="<?php echo get_template_directory_uri() ?>/img/raabit_haken_aufzaehlung.svg" /> <?php the_field('rufbereitschaft_haken4'); ?>
-		</div>
-	</div>
-	<div class="grid-x grid-padding-y mail-phone">
-		<div class="cell small-2 large-offset-4 text-center border-right">
-			<img src="<?php echo get_template_directory_uri() ?>/img/mail-icon.png" /><br />
-			<a href="mailto:info@Raab-IT.de">info@Raab-IT.de</a>
-		</div>
-		<div class="cell small-2 text-center">
-			<img src="<?php echo get_template_directory_uri() ?>/img/phone-icon.png" /><br />
-			<a href="call:+498001013398">08001013398</a>
-		</div>
-	</div>
-</div>
-
-<!-- Klaus Raab -->
-<div class="bg-light-grey">
-	<div class="grid-container">
-		<div class="grid-x grid-padding-x align-middle klaus-raab">
-			<div class="cell small-7">
-				<?php echo wp_get_attachment_image( get_field('zitat_bild'), 'full' ); ?>
-			</div>
-			<div class="cell small-5 padding-small quote">
-				<p><?php the_field('rufbereitschaft_haken3'); ?></p>
-				<strong>Klaus Raab, Geschäftsführer</strong>
-			</div>
-		</div>
-	</div>
-</div>
-
-<!-- Benutzerservice -->
-<div class="bg-light-grey">
-	<div class="grid-container benutzerservice">
-		<div class="grid-x align-center padding-x-80">
-			<div class="cell small-8 text-center">
-				<h3 class="color-red"><?php the_field('benutzerservice_headline'); ?></h3>
-				<p><?php the_field('benutzerservice_text'); ?></p>
-			</div>
-		</div>
-		<div class="grid-x grid-padding-y grid-margin-x check-row">
-			<div class="cell small-4 large-offset-2 border-bottom-red">
-				<img class="check" src="<?php echo get_template_directory_uri() ?>/img/raabit_haken_aufzaehlung.svg" /> <?php the_field('Benutzerservice_haken1'); ?>
-			</div>
-			<div class="cell small-4  border-bottom-red">
-				<img class="check" src="<?php echo get_template_directory_uri() ?>/img/raabit_haken_aufzaehlung.svg" /> <?php the_field('Benutzerservice_haken2'); ?>
-			</div>
-		</div>
-		<div class="grid-x grid-padding-y grid-margin-x check-row">
-			<div class="cell small-4 large-offset-2">
-				<img class="check" src="<?php echo get_template_directory_uri() ?>/img/raabit_haken_aufzaehlung.svg" /> <?php the_field('Benutzerservice_haken3'); ?>
-			</div>
-			<div class="cell small-4">
-				<img class="check" src="<?php echo get_template_directory_uri() ?>/img/raabit_haken_aufzaehlung.svg" /> <?php the_field('Benutzerservice_haken4'); ?>
-			</div>
-		</div>
+		<?php
+		else :
+		endif;
+		?>
+		
 	</div>
 </div>
 
