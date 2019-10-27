fd7f9c28765404bab7a1b8601033743813514034
JB <info@jonasbuck.com>
2019-10-27 20:35:08 +0100

    Karriere H3 red

--- page-templates/karriere.php
+++ page-templates/karriere.php
@@ -61,7 +61,7 @@ $full_bg_img = wp_get_attachment_image_src( get_field('karriere_header'), "full"
 				<?php endwhile; ?>
 			</ul>
 			
-			<h4><?php the_field('erwartungen_header'); ?></h4>
+			<h3 class="color-red"><?php the_field('erwartungen_header'); ?></h3>
 			<?php
 			if( have_rows('erwartungen') ):
 				while ( have_rows('erwartungen') ) : the_row(); ?>
