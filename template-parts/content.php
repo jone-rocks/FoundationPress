6a69fa2c4ad881c106aa0f19f813250e63659226
JB <info@jonasbuck.com>
2020-02-23 17:12:36 +0100

    Blog

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,30 +10,29 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header>
-	<?php
+<div class="grid-container" id="post-<?php the_ID(); ?>">
+	<div class="grid-x align-center padding-top-80">
+		<div class="cell small-12 medium-8 text-center text-center-mobile">
+			
+			<?php
 		if ( is_single() ) {
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		} else {
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			the_title( '<h3 class="entry-title color-red">', '</h3>' );
 		}
 	?>
+			
+			
 		<?php foundationpress_entry_meta(); ?>
-	</header>
+
 	<div class="entry-content">
 		<?php the_content(); ?>
 		<?php edit_post_link( __( '(Edit)', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
 	</div>
-	<footer>
-		<?php
-			wp_link_pages(
-				array(
-					'before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ),
-					'after'  => '</p></nav>',
-				)
-			);
-		?>
-		<?php $tag = get_the_tags(); if ( $tag ) { ?><p><?php the_tags(); ?></p><?php } ?>
-	</footer>
-</article>
+
+
+
+
+
+
+		</div>
+	</div>
+</div>
